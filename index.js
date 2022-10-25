d86e363b031f08dfd7ab833bc39639cf5de9d6b6
MushfiqMoon <mushfiq.moon013@google.com>
2022-10-25 22:30:28 +0600

    game devlopment data added

--- index.js
+++ index.js
@@ -7,14 +7,36 @@ app.use(cors())
 
 
 const categories = require('./data/categories.json')
+const courses = require('./data/courses.json')
 
 app.get('/', (req, res) => {
-    res.send('news api running')
+    res.send('Courses api running')
 })
 
+// for all Category API
 app.get('/categories', (req, res) => {
     res.send(categories)
 })
+
+// for single course API 
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id
+    const selestedCourse = courses.find(cours => cours.id === id)
+    res.send(selestedCourse)
+})
+
+// for category wise course API 
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id
+    const selestedCategory = courses.filter(cours => cours.category_id === id)
+    res.send(selestedCategory)
+})
+
+// for all course API
+app.get('/courses/all', (req, res) => {
+    res.send(courses)
+})
+
 
 app.listen(port, () => {
     console.log(`Running on Port ${port}`)
