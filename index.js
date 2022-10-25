436cdf9430962f707a092790e425009f6a6e24a5
MushfiqMoon <mushfiq.moon013@google.com>
2022-10-25 17:03:07 +0600

    setup nodemon and package config

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
-
 const port = process.env.PORT || 5000
+
+app.use(cors())
+
 
 const categories = require('./data/categories.json')
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('news api running')
 })
 
-app.get('/categories', (req,res)=>{
+app.get('/categories', (req, res) => {
     res.send(categories)
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Running on Port ${port}`)
 })
